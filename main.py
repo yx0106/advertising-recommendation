58229906558928c6efbb4cb36225ead2af8e0d8d
GOH YEE XIAN <1171201118@student.mmu.edu.my>
2021-10-26 13:42:46 +0800

    add cleardb

--- main.py
+++ main.py
@@ -2,9 +2,51 @@ from flask import Flask, render_template, request, redirect, flash
 from experta import *
 import pymysql
 import pandas as pd
-from db_conn import *
+# from db_conn import *
 import json
-from sqlalchemy import create_engine
+import sqlalchemy
+
+# conn = pymysql.connect("us-cdbr-east-04.cleardb.com","b684dfd22ac1cf","17a17075","heroku_0bce2225ad59418" )
+conn = sqlalchemy.create_engine("mysql+pymysql://b684dfd22ac1cf:17a17075@us-cdbr-east-04.cleardb.com/heroku_0bce2225ad59418")
+
+# SQL_Query = pd.read_sql_query('''select * from signage_details''', conn)
+dataset = pd.read_sql_table('signage_details', conn)  
+# dataset = pd.DataFrame(SQL_Query, columns=['signage_id', 'day', 'name', 'address', 'state', 'popular_time', 'hour', 'weekend'])
+# dataset = pd.read_csv("https://github.com/yx0106/mmu-deg-fyp/blob/main/dataset.csv") 
+import numpy as np
+allState = dataset['state'].unique()
+stateList = dataset['state'].unique()
+for x in range(len(allState)):
+    allState[x] = dataset.loc[(dataset['state'].str.contains(allState[x])) & (dataset["weekend"] == 1)]
+    allState[x] = allState[x].sort_values(by=['popular_time'], ascending=False)
+    allState[x] = allState[x]['signage_id'].unique().tolist()
+stateList = stateList.reshape((len(stateList),1))   
+allState = allState.reshape((len(allState),1))
+State_list_no_weekend = np.concatenate((stateList,allState), axis = 1)
+
+df1 = pd.DataFrame(State_list_no_weekend, 
+                  columns=['state', 
+                      'signage_id'])
+
+df1['length'] = df1['signage_id'].apply(len)
+
+import numpy as np
+allState = dataset['state'].unique()
+stateList = dataset['state'].unique()
+for x in range(len(allState)):
+    allState[x] = dataset.loc[(dataset['state'].str.contains(allState[x])) & (dataset["weekend"] == 0)]
+    allState[x] = allState[x].sort_values(by=['popular_time'], ascending=False)
+    allState[x] = allState[x]['signage_id'].unique().tolist()
+stateList = stateList.reshape((len(stateList),1))   
+allState = allState.reshape((len(allState),1))
+State_list_no_weekend = np.concatenate((stateList,allState), axis = 1)
+
+df2 = pd.DataFrame(State_list_no_weekend, 
+                  columns=['state', 
+                      'signage_id'])
+
+df2['length'] = df2['signage_id'].apply(len)
+
 
 signage_name = ''
 signage_address = ''
